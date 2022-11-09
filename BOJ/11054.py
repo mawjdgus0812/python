7dc30602d1820fb2d43f4c69865e3d8a99f34551
mawjdgus0812 <mawjdgus@gmail.com>
2022-11-09 20:18:57 +0900

    number

--- BOJ/11054.py
+++ BOJ/11054.py
@@ -9,21 +9,6 @@ dp_reverse = [0] * N
 dp[0] = 1
 dp_reverse[0] = 1
 
-# for i in range(1,N):
-#     m = 0
-#     m_2 =0
-#     for j in range(i):
-#         if arr[i] > arr[j]:
-#             m = max(dp[j],m)
-#         dp[i] = m + 1
-#         if arr_reverse[i] > arr_reverse[j]:
-#             m_2 = max(dp_reverse[j], m_2)
-#         dp_reverse[i] = m_2 + 1
-
-# sum = [x + y for (x,y) in zip(dp,dp_reverse[::-1])]
-# # print(dp, dp_reverse)
-# print(max(sum)-1)
-
 def dynamic(dp,arr):
     for i in range(1,N):
         m = 0
